2e972d4671099c88b8ae44f6fb3193c79ef6006b
Manish Tripathi <manish82644@gmail.com>
2024-06-11 02:56:47 +0530

    Added book page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/book-page', function () {
+    return view('pages.book');
+})->name('book-page');
