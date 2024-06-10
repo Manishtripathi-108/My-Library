45521a1edcfe4f56d5ef2d74d7e785a248a11d9b
Manish Tripathi <manish82644@gmail.com>
2024-06-11 02:46:39 +0530

    chore: Add initial project files and configurations

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $title ?? config('app.name', 'My Component Library') }}</title>
+
+    <!-- Favicon and Apple Touch Icons -->
+    <link href="{{ asset('apple-touch-icon.png') }}" rel="apple-touch-icon" sizes="180x180">
+    <link type="image/png" href="{{ asset('favicon-32x32.png') }}" rel="icon" sizes="32x32">
+    <link type="image/png" href="{{ asset('favicon-16x16.png') }}" rel="icon" sizes="16x16">
+    <link href="{{ asset('site.webmanifest') }}" rel="manifest">
+    <link href="{{ asset('safari-pinned-tab.svg') }}" rel="mask-icon" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    {{-- styles --}}
+    @stack('styles')
+
+</head>
+
+<body class="font-sans antialiased">
+    {{-- Page Content --}}
+    <main>
+        {{ $slot }}
+    </main>
+
+    @stack('scripts')
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+<x-app-layout title="Welcome Page">
+    <h1>Welcome to My Component Library</h1>
+    <p>This is the content of the welcome page.</p>
+</x-app-layout>
