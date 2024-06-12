cd4495d34bb08a1c799cdcbf54372deb4f9a7922
Manish Tripathi <manish82644@gmail.com>
2024-06-12 23:30:19 +0530

    feat: Add reflect-hover card component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout title="Welcome Page">
     <h1>Welcome to My Component Library</h1>
     <p>This is the content of the welcome page.</p>
+    @include('cards.reflect-hover')
 </x-app-layout>
