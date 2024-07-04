39d8930731c3c0d47a5783981d394d52960a9a3e
Manish Tripathi <manish82644@gmail.com>
2024-07-05 01:06:32 +0530

    Refactor neumorphism styles for dashboard and navigation components

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -30,7 +30,7 @@ new class extends Component {
 
 				<!-- Logo -->
 				<div class="flex shrink-0 items-center">
-					<a href="{{ route('dashboard') }}" wire:navigate>
+					<a href="{{ route('home') }}" wire:navigate>
 						<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-dark-text-secondary" />
 					</a>
 				</div>
