093e2856f1f5406cb4ecb1a2fe3e4b5ea5eab718
Manish Tripathi <manish82644@gmail.com>
2024-07-05 01:02:36 +0530

    chore: Refactor neumorphism styles for input components and profile page

--- resources/views/livewire/profile/update-profile-information-form.blade.php
+++ resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -6,14 +6,13 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rule;
 use Livewire\Volt\Component;
 
-new class extends Component
-{
+new class extends Component {
     public string $name = '';
     public string $email = '';
 
     /**
-     * Mount the component.
-     */
+				 * Mount the component.
+				 */
     public function mount(): void
     {
         $this->name = Auth::user()->name;
@@ -21,8 +20,8 @@ new class extends Component
     }
 
     /**
-     * Update the profile information for the currently authenticated user.
-     */
+				 * Update the profile information for the currently authenticated user.
+				 */
     public function updateProfileInformation(): void
     {
         $user = Auth::user();
@@ -44,8 +43,8 @@ new class extends Component
     }
 
     /**
-     * Send an email verification notification to the current user.
-     */
+				 * Send an email verification notification to the current user.
+				 */
     public function sendVerification(): void
     {
         $user = Auth::user();
@@ -60,56 +59,57 @@ new class extends Component
 
         Session::flash('status', 'verification-link-sent');
     }
-}; ?>
+};
+?>
 
 <section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-dark-text">
-            {{ __('Profile Information') }}
-        </h2>
+	<header>
+		<h2 class="text-lg font-medium text-gray-900 dark:text-dark-text">
+			{{ __('Profile Information') }}
+		</h2>
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __("Update your account's profile information and email address.") }}
-        </p>
-    </header>
+		<p class="text-secondary mt-1 text-sm">
+			{{ __("Update your account's profile information and email address.") }}
+		</p>
+	</header>
 
-    <form wire:submit="updateProfileInformation" class="mt-6 space-y-6">
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" name="name" type="text" class="mt-1 block w-full" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
-        </div>
+	<form class="mt-6 space-y-6" wire:submit="updateProfileInformation">
+		<div>
+			<x-input-label for="name" :value="__('Name')" />
+			<x-text-input class="mt-1 block w-full" id="name" name="name" type="text" wire:model="name" required autofocus autocomplete="name" />
+			<x-input-error class="mt-2" :messages="$errors->get('name')" />
+		</div>
 
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" name="email" type="email" class="mt-1 block w-full" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
+		<div>
+			<x-input-label for="email" :value="__('Email')" />
+			<x-text-input class="mt-1 block w-full" id="email" name="email" type="email" wire:model="email" required autocomplete="username" />
+			<x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-            @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! auth()->user()->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800 dark:text-dark-text-secondary">
-                        {{ __('Your email address is unverified.') }}
+			@if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !auth()->user()->hasVerifiedEmail())
+				<div>
+					<p class="mt-2 text-sm text-gray-800 dark:text-dark-text-secondary">
+						{{ __('Your email address is unverified.') }}
 
-                        <button wire:click.prevent="sendVerification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
+						<button class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800" wire:click.prevent="sendVerification">
+							{{ __('Click here to re-send the verification email.') }}
+						</button>
+					</p>
 
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
-        </div>
+					@if (session('status') === 'verification-link-sent')
+						<p class="mt-2 text-sm font-medium text-green-600 dark:text-green-400">
+							{{ __('A new verification link has been sent to your email address.') }}
+						</p>
+					@endif
+				</div>
+			@endif
+		</div>
 
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+		<div class="flex items-center gap-4">
+			<x-primary-button>{{ __('Save') }}</x-primary-button>
 
-            <x-action-message class="me-3" on="profile-updated">
-                {{ __('Saved.') }}
-            </x-action-message>
-        </div>
-    </form>
+			<x-action-message class="me-3" on="profile-updated">
+				{{ __('Saved.') }}
+			</x-action-message>
+		</div>
+	</form>
 </section>
