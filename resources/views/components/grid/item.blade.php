697732eff69ff0eefe1a7b081d1a3a4ffb02d0fd
Manish Tripathi <manish82644@gmail.com>
2024-07-07 20:38:41 +0530

    refactor: Update grid item title font color

--- resources/views/components/grid/item.blade.php
+++ resources/views/components/grid/item.blade.php
@@ -2,7 +2,7 @@
 
 <div {{ $attributes->class(['col-span-1 flex w-full flex-col items-center justify-center lg:col-span-2', 'shadow-neu-inset p-4' => $isNeumorphism]) }}>
 	{{ $slot }}
-	<h3 class="font-karla mt-3 text-center text-xl tracking-wider">
+	<h3 class="font-karla mt-4 text-center text-xl tracking-wider text-secondary">
 		{{ ucwords($title) }}
 	</h3>
 </div>
