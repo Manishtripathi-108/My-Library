3876fd6cb9e0f4ae9b0265f7380d9f8f42c2b75a
Manish Tripathi <manish82644@gmail.com>
2024-07-07 17:50:00 +0530

    refactor: updated layout of clock page

--- resources/views/neumorphism/clock.blade.php
+++ resources/views/neumorphism/clock.blade.php
@@ -5,59 +5,69 @@
 		</h2>
 	</x-slot>
 
-	<div class="h-dvh grid-center w-full grid-cols-3 gap-10">
-		<div class="flex-center w-full">
-			<div class="size-80 flex-center before:size-4 bg-clock rounded-full border-4 border-solid border-light-primary shadow-neu-inset-md before:z-50 before:rounded-full before:bg-dark-secondary dark:border-dark-primary dark:shadow-neu-dark-inset-md">
-				<div class="size-40 absolute">
-					<div class="size-40 hr flex justify-center rounded-full before:absolute before:z-10 before:h-20 before:w-2 before:rounded-t-full before:bg-dark-primary dark:before:bg-light-secondary"></div>
-				</div>
-				<div class="size-52 absolute">
-					<div class="size-52 mn flex justify-center rounded-full before:absolute before:z-20 before:h-24 before:w-1 before:rounded-t-full before:bg-dark-secondary"></div>
-				</div>
-				<div class="size-60 absolute">
-					<div class="size-60 sc flex justify-center rounded-full before:absolute before:z-30 before:h-36 before:w-0.5 before:rounded-t-full before:bg-dark-secondary"></div>
+	<x-grid title="Clocks">
+
+        {{-- Simple Clock --}}
+		<x-grid.item title="Simple Clock">
+			<div class="flex-center w-full">
+				<div class="size-80 flex-center text-secondary relative rounded-full border-2 border-light-primary text-xl shadow-neu-inset-md dark:border-dark-primary dark:shadow-neu-dark-inset-md [&>*:nth-child(3n)]:text-4xl [&>*:nth-child(3n)]:font-bold" id="clock">
+
+					@for ($i = 1; $i <= 12; $i++)
+						<div class="absolute h-[95%] font-playfair" id="{{ $i }}" style="transform: rotateZ({{ $i * 30 }}deg);">{{ $i }}</div>
+					@endfor
+
 				</div>
 			</div>
-		</div>
+		</x-grid.item>
 
-		<div class="flex-center w-full">
-			<div class="size-80 flex-center text-secondary relative rounded-full text-xl shadow-neu-inset-md [&>*:nth-child(3n)]:text-4xl [&>*:nth-child(3n)]:font-bold" id="clock">
-
-				@for ($i = 1; $i <= 12; $i++)
-					<div class="absolute h-[95%] font-playfair" id="{{ $i }}">{{ $i }}</div>
-				@endfor
-                
+        {{-- Clock with Working Hands and image for dial--}}
+		<x-grid.item title="Clock with Working Hands and dial image">
+			<div class="flex-center w-full">
+				<div class="size-80 flex-center before:size-4 bg-clock rounded-full border-4 border-solid border-light-primary shadow-neu-inset-md before:z-50 before:rounded-full before:bg-dark-secondary dark:border-dark-primary dark:shadow-neu-dark-inset-md">
+					<div class="size-40 absolute">
+						<div class="size-40 hr flex justify-center rounded-full before:absolute before:z-10 before:h-20 before:w-2 before:rounded-t-full before:bg-dark-primary dark:before:bg-light-secondary"></div>
+					</div>
+					<div class="size-52 absolute">
+						<div class="size-52 mn flex justify-center rounded-full before:absolute before:z-20 before:h-24 before:w-1 before:rounded-t-full before:bg-dark-secondary"></div>
+					</div>
+					<div class="size-60 absolute">
+						<div class="size-60 sc flex justify-center rounded-full before:absolute before:z-30 before:h-36 before:w-0.5 before:rounded-t-full before:bg-dark-secondary"></div>
+					</div>
+				</div>
 			</div>
-		</div>
+		</x-grid.item>
 
-		<div class="flex-center w-full">
-			<div class="size-80 before:size-4 flex-center text-secondary relative rounded-full border-4 border-solid border-light-primary text-xl shadow-neu-inset-md before:z-50 before:rounded-full before:bg-dark-secondary dark:border-dark-primary dark:shadow-neu-dark-inset-md [&>*:nth-child(3n)]:text-4xl [&>*:nth-child(3n)]:font-bold" id="clock">
+        {{-- Clock with Working Hands and Numbers --}}
+		<x-grid.item title="Clock with Working Hands and Numbers">
+			<div class="flex-center w-full">
+				<div class="size-80 before:size-4 flex-center text-secondary relative rounded-full border-4 border-solid border-light-primary text-xl shadow-neu-inset-md before:z-50 before:rounded-full before:bg-dark-secondary dark:border-dark-primary dark:shadow-neu-dark-inset-md [&>*:nth-child(3n)]:text-4xl [&>*:nth-child(3n)]:font-bold" id="clock">
 
-				@for ($i = 1; $i <= 12; $i++)
-					<div class="absolute h-[95%] font-playfair" id="{{ $i }}">{{ $i }}</div>
-				@endfor
+					@for ($i = 1; $i <= 12; $i++)
+						<div class="absolute h-[95%] font-playfair" id="{{ $i }}" style="transform: rotateZ({{ $i * 30 }}deg);">{{ $i }}</div>
+					@endfor
 
-				<div class="size-40 absolute">
-					<div class="size-40 hr flex justify-center rounded-full before:absolute before:z-10 before:h-20 before:w-2 before:rounded-t-full before:bg-dark-primary dark:before:bg-light-secondary"></div>
+					<div class="size-40 absolute">
+						<div class="size-40 hr flex justify-center rounded-full before:absolute before:z-10 before:h-20 before:w-2 before:rounded-t-full before:bg-dark-primary dark:before:bg-light-secondary"></div>
+					</div>
+					<div class="size-52 absolute">
+						<div class="size-52 mn flex justify-center rounded-full before:absolute before:z-20 before:h-24 before:w-1 before:rounded-t-full before:bg-dark-secondary"></div>
+					</div>
+					<div class="size-60 absolute">
+						<div class="size-60 sc flex justify-center rounded-full before:absolute before:z-30 before:h-36 before:w-0.5 before:rounded-t-full before:bg-dark-secondary"></div>
+					</div>
 				</div>
-				<div class="size-52 absolute">
-					<div class="size-52 mn flex justify-center rounded-full before:absolute before:z-20 before:h-24 before:w-1 before:rounded-t-full before:bg-dark-secondary"></div>
-				</div>
-				<div class="size-60 absolute">
-					<div class="size-60 sc flex justify-center rounded-full before:absolute before:z-30 before:h-36 before:w-0.5 before:rounded-t-full before:bg-dark-secondary"></div>
-				</div>
-
 			</div>
-		</div>
-	</div>
+		</x-grid.item>
+
+	</x-grid>
 
 	@pushOnce('scripts')
 		<script>
 			// for loop to create the clock
-			for (let i = 1; i <= 12; i++) {
-				const id = document.getElementById(i);
-				id.style.transform = `rotateZ(${i * 30}deg)`;
-			}
+			// for (let i = 1; i <= 12; i++) {
+			// 	const id = document.getElementById(i);
+			// 	id.style.transform = `rotateZ(${i * 30}deg)`;
+			// }
 
 			// function to run the clock
 			function clock() {
