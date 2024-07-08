783f44b0ca9f459b05b734d99e7cba8ffb1fe242
Manish Tripathi <manish82644@gmail.com>
2024-07-08 11:29:59 +0530

    refactor: Update radio button styles with hidden input for accessibility

--- resources/views/neumorphism/radio-btn.blade.php
+++ resources/views/neumorphism/radio-btn.blade.php
@@ -1,28 +1,28 @@
 <div class="bg-primary rounded-2xl px-16 py-12 shadow-neu-inset-xs dark:shadow-neu-dark-inset-xs">
 	<div class="mx-0 my-2">
-		<label class="text-secondary group/label inline-flex cursor-pointer items-center" for="a">
-			<input class="peer/state absolute right-0 top-0" id="a" name="app" type="radio" value="a">
+		<label class="text-secondary group/label relative inline-flex cursor-pointer items-center" for="a">
+			<input class="peer/state hidden" id="a" name="app" type="radio" value="a">
 			<div class="size-8 before:size-4/5 relative overflow-hidden rounded-full shadow-neu-xs before:absolute before:left-[10%] before:top-[10%] before:rounded-full before:shadow-neu-md-soft peer-checked/state:before:shadow-neu-inset-xs dark:shadow-neu-dark-xs dark:before:shadow-neu-dark-md dark:peer-checked/state:before:shadow-neu-dark-inset-xs"></div>
 			<span class="ml-4 font-karla opacity-60 transition-all group-hover/label:translate-x-2 group-hover/label:opacity-100 peer-checked/state:translate-x-2 peer-checked/state:opacity-100 peer-focus/state:opacity-100">close</span>
 		</label>
 	</div>
 	<div class="mx-0 my-2">
-		<label class="text-secondary group/label inline-flex cursor-pointer items-center" for="b">
-			<input class="peer/state absolute right-0 top-0" id="b" name="app" type="radio" value="b">
+		<label class="text-secondary group/label relative inline-flex cursor-pointer items-center" for="b">
+			<input class="peer/state hidden" id="b" name="app" type="radio" value="b">
 			<div class="size-8 before:size-4/5 relative overflow-hidden rounded-full shadow-neu-xs before:absolute before:left-[10%] before:top-[10%] before:rounded-full before:shadow-neu-md-soft peer-checked/state:before:shadow-neu-inset-xs dark:shadow-neu-dark-xs dark:before:shadow-neu-dark-md dark:peer-checked/state:before:shadow-neu-dark-inset-xs"></div>
 			<span class="ml-4 font-karla opacity-60 transition-all group-hover/label:translate-x-2 group-hover/label:opacity-100 peer-checked/state:translate-x-2 peer-checked/state:opacity-100 peer-focus/state:opacity-100">remove</span>
 		</label>
 	</div>
 	<div class="mx-0 my-2">
-		<label class="text-secondary group/label inline-flex cursor-pointer items-center" for="c">
-			<input class="peer/state absolute right-0 top-0" id="c" name="app" type="radio" value="c">
+		<label class="text-secondary group/label relative inline-flex cursor-pointer items-center" for="c">
+			<input class="peer/state hidden" id="c" name="app" type="radio" value="c">
 			<div class="size-8 before:size-4/5 relative overflow-hidden rounded-full shadow-neu-xs before:absolute before:left-[10%] before:top-[10%] before:rounded-full before:shadow-neu-md-soft peer-checked/state:before:shadow-neu-inset-xs dark:shadow-neu-dark-xs dark:before:shadow-neu-dark-md dark:peer-checked/state:before:shadow-neu-dark-inset-xs"></div>
 			<span class="ml-4 font-karla opacity-60 transition-all group-hover/label:translate-x-2 group-hover/label:opacity-100 peer-checked/state:translate-x-2 peer-checked/state:opacity-100 peer-focus/state:opacity-100">delete</span>
 		</label>
 	</div>
 	<div class="mx-0 my-2">
-		<label class="text-secondary group/label inline-flex cursor-pointer items-center" for="d">
-			<input class="peer/state absolute right-0 top-0" id="d" name="app" type="radio" value="d">
+		<label class="text-secondary group/label relative inline-flex cursor-pointer items-center" for="d">
+			<input class="peer/state hidden" id="d" name="app" type="radio" value="d">
 			<div class="size-8 before:size-4/5 relative overflow-hidden rounded-full shadow-neu-xs before:absolute before:left-[10%] before:top-[10%] before:rounded-full before:shadow-neu-md-soft peer-checked/state:before:shadow-neu-inset-xs dark:shadow-neu-dark-xs dark:before:shadow-neu-dark-md dark:peer-checked/state:before:shadow-neu-dark-inset-xs"></div>
 			<span class="ml-4 font-karla opacity-60 transition-all group-hover/label:translate-x-2 group-hover/label:opacity-100 peer-checked/state:translate-x-2 peer-checked/state:opacity-100 peer-focus/state:opacity-100">all of the above</span>
 		</label>
