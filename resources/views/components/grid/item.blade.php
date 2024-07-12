cd77d8419fbad01f32504353ef7959956110cf29
Manish Tripathi <manish82644@gmail.com>
2024-07-12 15:42:54 +0530

    refactor: added alpine to code showing and code changing

--- resources/views/components/grid/item.blade.php
+++ resources/views/components/grid/item.blade.php
@@ -1,34 +1,36 @@
 @props(['title' => '', 'isNeumorphism' => false])
 
-<div {{ $attributes->class(['col-span-1 w-full mt-10 border-b border-gray-600 pb-5', 'shadow-neu-inset-sm dark:shadow-neu-dark-inset-sm p-4' => $isNeumorphism]) }}>
+<div x-data="{ code_open: false }" {{ $attributes->class(['col-span-1 w-full mt-10 border-b border-gray-600 pb-5', 'shadow-neu-inset-sm dark:shadow-neu-dark-inset-sm p-4' => $isNeumorphism]) }}>
 	<h3 class="text-secondary mb-8 font-alegreya text-2xl font-bold tracking-wider">
 		{{ ucwords($title) }}
 	</h3>
 
-	<div class="flex-center flex-col rounded-lg border border-gray-600 pt-5">
+	<div class="flex-center flex-col rounded-lg border border-gray-600 px-5 pt-5">
 
 		{{ $slot }}
 
+		{{-- Show Code Button --}}
 		<div class="flex-center mt-5 w-full border-t border-gray-600 p-5">
-			<button class="neu-btn neu-icon-btn text-xs">
+			<button class="neu-btn neu-icon-btn text-xs" :class="{ 'active': code_open }" @click="code_open = !code_open">
 				<x-svg.lock class="size-5" />
 				SHOW CODE
 			</button>
 		</div>
 	</div>
 
-	<div class="mt-5 rounded-lg border border-gray-600 pb-5">
+	{{-- Code Tabs --}}
+	<div class="mx-auto mt-5 w-[60vw] rounded-lg border border-gray-600 pb-5" x-data="{ code_tab: 'html-code' }" x-show="code_open" x-transition>
 		<div class="mb-5 flex w-full items-center gap-4 border-b border-gray-600 p-5">
-			<button class="neu-btn text-xs">
+			<button class="neu-btn text-xs" :class="{ 'active': code_tab === 'html-code' }" @click="code_tab = 'html-code'">
 				HTML
 			</button>
-			<button class="neu-btn text-xs">
+			<button class="neu-btn text-xs" :class="{ 'active': code_tab === 'css-code' }" @click="code_tab = 'css-code'">
 				CSS
 			</button>
 		</div>
 
 		<div class="p-5">
-			<pre class="text-secondary scrollbar-thin w-[60vw] overflow-x-auto text-sm leading-normal">
+			<pre class="text-secondary scrollbar-thin overflow-x-auto text-sm leading-normal" x-show="code_tab === 'html-code'">
 			  @php
 				echo htmlspecialchars(
 				    '<div class="flex-center thin-scrollbar mt-5 w-full border-t border-gray-600 p-5">
@@ -44,7 +46,11 @@
 				);
 			@endphp
 			</pre>
-		</div>
 
+			<pre class="text-secondary scrollbar-thin overflow-x-auto text-sm leading-normal" x-show="code_tab === 'css-code'">
+            /* CSS code goes here */
+        	</pre>
+		</div>
 	</div>
+
 </div>
