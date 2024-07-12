2b2d1453906d701284ce249d73c44a2d41f45fdd
Manish Tripathi <manish82644@gmail.com>
2024-07-12 16:21:14 +0530

    refactor: completed the grid main layout

--- resources/views/components/grid/index.blade.php
+++ resources/views/components/grid/index.blade.php
@@ -1,11 +1,11 @@
-@props(['title' => ''])
+@props(['title' => '', 'description' => ''])
 
 <section class="w-full">
 	<h2 class="text-secondary mb-5 font-playfair text-2xl font-bold tracking-wider">
 		{{ ucwords($title) }}
 	</h2>
 	<div class="text-secondary font-karla text-sm">
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae aspernatur quam minima voluptas excepturi expedita neque saepe ullam culpa hic quo dolorem, natus eos nobis. In ducimus voluptatem doloribus!
+		{{ $description }}
 	</div>
 
 	<div class="mb-2 grid place-items-center gap-4 p-3">
--- resources/views/components/grid/item.blade.php
+++ resources/views/components/grid/item.blade.php
@@ -1,56 +1,48 @@
-@props(['title' => '', 'isNeumorphism' => false])
+@props(['title' => '', 'isNeumorphism' => false, 'htmlCode' => '', 'cssCode' => ''])
 
-<div x-data="{ code_open: false }" {{ $attributes->class(['col-span-1 w-full mt-10 border-b border-gray-600 pb-5', 'shadow-neu-inset-sm dark:shadow-neu-dark-inset-sm p-4' => $isNeumorphism]) }}>
+<div x-data="{ code_open: false }" {{ $attributes->class(['col-span-1 w-full mt-10 border-b border-gray-600 pb-10', 'shadow-neu-inset-sm dark:shadow-neu-dark-inset-sm p-4' => $isNeumorphism]) }}>
 	<h3 class="text-secondary mb-8 font-alegreya text-2xl font-bold tracking-wider">
 		{{ ucwords($title) }}
 	</h3>
 
-	<div class="flex-center flex-col rounded-lg border border-gray-600 px-5 pt-5">
+	<div class="flex-center flex-col rounded-lg border border-gray-600 p-5">
 
 		{{ $slot }}
 
 		{{-- Show Code Button --}}
-		<div class="flex-center mt-5 w-full border-t border-gray-600 p-5">
-			<button class="neu-btn neu-icon-btn text-xs" :class="{ 'active': code_open }" @click="code_open = !code_open">
-				<x-svg.lock class="size-5" />
-				SHOW CODE
-			</button>
-		</div>
+		@if ($htmlCode != '' || $cssCode != '')
+			<div class="flex-center mt-5 w-full border-t border-gray-600 pt-5">
+				<button class="neu-btn neu-icon-btn text-xs" :class="{ 'active': code_open }" @click="code_open = !code_open">
+					<x-svg.lock class="size-5" />
+					SHOW CODE
+				</button>
+			</div>
+		@endif
 	</div>
 
-	{{-- Code Tabs --}}
-	<div class="mx-auto mt-5 w-[60vw] rounded-lg border border-gray-600 pb-5" x-data="{ code_tab: 'html-code' }" x-show="code_open" x-transition>
-		<div class="mb-5 flex w-full items-center gap-4 border-b border-gray-600 p-5">
-			<button class="neu-btn text-xs" :class="{ 'active': code_tab === 'html-code' }" @click="code_tab = 'html-code'">
-				HTML
-			</button>
-			<button class="neu-btn text-xs" :class="{ 'active': code_tab === 'css-code' }" @click="code_tab = 'css-code'">
-				CSS
-			</button>
-		</div>
-
-		<div class="p-5">
-			<pre class="text-secondary scrollbar-thin overflow-x-auto text-sm leading-normal" x-show="code_tab === 'html-code'">
-			  @php
-				echo htmlspecialchars(
-				    '<div class="flex-center thin-scrollbar mt-5 w-full border-t border-gray-600 p-5">
-					<button class="neu-btn neu-icon-btn text-xs">
-					  <x-svg.lock class="size-5" />
-					  SHOW CODE
+	@if ($htmlCode != '' || $cssCode != '')
+		{{-- Code Tabs --}}
+		<div class="mx-auto mt-5 w-[60vw] rounded-lg border border-gray-600 pb-5" x-data="{ code_tab: 'html-code' }" x-show="code_open" x-transition.duration.300ms>
+			<div class="mb-5 flex w-full items-center gap-4 border-b border-gray-600 p-5">
+				<button class="neu-btn text-xs" :class="{ 'active': code_tab === 'html-code' }" @click="code_tab = 'html-code'">
+					HTML
+				</button>
+				@if ($cssCode != '')
+					<button class="neu-btn text-xs" :class="{ 'active': code_tab === 'css-code' }" @click="code_tab = 'css-code'">
+						CSS
 					</button>
-				  </div>
-				  lorem 5 lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quae aspernatur quam minima voluptas excepturi expedita neque saepe ullam culpa hic quo dolorem, natus eos nobis. In ducimus voluptatem doloribus!
-				  ',
-				    ENT_QUOTES,
-				    'UTF-8',
-				);
-			@endphp
-			</pre>
+				@endif
+			</div>
 
-			<pre class="text-secondary scrollbar-thin overflow-x-auto text-sm leading-normal" x-show="code_tab === 'css-code'">
-            /* CSS code goes here */
-        	</pre>
+			<div class="scrollbar-thin overflow-x-auto p-5">
+				{{-- HTML Code --}}
+				<pre class="text-secondary text-sm leading-normal" x-show="code_tab === 'html-code'">{{ e($htmlCode) }}</pre>
+
+				{{-- CSS Code --}}
+				@if ($cssCode != '')
+					<pre class="text-secondary text-sm leading-normal" x-show="code_tab === 'css-code'">{{ e($cssCode) }}</pre>
+				@endif
+			</div>
 		</div>
-	</div>
-
+	@endif
 </div>
