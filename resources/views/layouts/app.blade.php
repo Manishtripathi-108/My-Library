16609d80e8ead21dd93d1c0273ed2163e24a1876
Manish Tripathi <manish82644@gmail.com>
2024-07-13 16:26:04 +0530

    refactor: Update grid item component to use slot for HTML code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
 		<!-- Page Heading -->
 		@if (isset($header))
 			<header class="bg-primary shadow dark:shadow-dark-secondary">
-				<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+				<div class="*:font-alegreya *:tracking-wide mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 					{{ $header }}
 				</div>
 			</header>
