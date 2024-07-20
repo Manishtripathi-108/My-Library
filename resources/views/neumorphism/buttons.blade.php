46d767a1e6f90052e883871a10d9ca9b12eb70b6
Manish Tripathi <manish82644@gmail.com>
2024-07-20 08:09:02 -0400

    refactor: Update neumorphism buttons styling and add Loading button

--- resources/views/neumorphism/buttons.blade.php
+++ resources/views/neumorphism/buttons.blade.php
@@ -8,23 +8,49 @@
 
 		<x-grid.item title="Buttons">
 			<div class="flex-center">
-				<button class="neu-btn text-primary mr-2" type="button">Primary</button>
-				<button class="neu-btn text-secondary mr-2" type="button">Secondary</button>
-				<button class="neu-btn mr-2 text-green-700" type="button">Success</button>
-				<button class="neu-btn mr-2 text-red-700" type="button">Danger</button>
-				<button class="neu-btn text-primary active mr-2" type="button" disabled>Active</button>
+				<button class="neu-btn text-primary mr-3" type="button">Primary</button>
+				<button class="neu-btn text-secondary mr-3" type="button">Secondary</button>
+				<button class="neu-btn mr-3 text-green-700" type="button">Success</button>
+				<button class="neu-btn mr-3 text-red-700" type="button">Danger</button>
+				<button class="neu-btn text-primary active mr-3" type="button">Active</button>
 			</div>
 		</x-grid.item>
 
-        <x-grid.item title="Button Pill">
-            <div class="flex-center">
-                <button class="neu-btn neu-btn-pill text-primary mr-2" type="button">Primary</button>
-                <button class="neu-btn neu-btn-pill text-secondary mr-2" type="button">Secondary</button>
-                <button class="neu-btn neu-btn-pill mr-2 text-green-700" type="button">Success</button>
-                <button class="neu-btn neu-btn-pill mr-2 text-red-700" type="button">Danger</button>
-                <button class="neu-btn neu-btn-pill text-primary active mr-2" type="button" disabled>Active</button>
-            </div>
-        </x-grid.item>
+		<x-grid.item title="Button Pill">
+			<div class="flex-center">
+				<button class="neu-btn neu-btn-pill text-primary mr-3" type="button">Primary</button>
+				<button class="neu-btn neu-btn-pill text-secondary mr-3" type="button">Secondary</button>
+				<button class="neu-btn neu-btn-pill mr-3 text-green-700" type="button">Success</button>
+				<button class="neu-btn neu-btn-pill mr-3 text-red-700" type="button">Danger</button>
+				<button class="neu-btn neu-btn-pill text-primary active mr-3" type="button">Active</button>
+			</div>
+		</x-grid.item>
+
+		<x-grid.item title="Only Icon Buttons">
+			<div class="flex-center">
+				<button class="neu-icon-only-btn neu-btn mr-3" type="button"><x-svg.sun class="size-7" /></button>
+				<button class="neu-icon-only-btn neu-btn active mr-6" type="button"><x-svg.truck class="size-7" /></button>
+				<button class="neu-icon-only-square-btn neu-btn mr-3" type="button"><x-svg.moon class="size-7" /> </button>
+				<button class="neu-icon-only-square-btn neu-btn active mr-3" type="button"><x-svg.mail class="size-7" /></button>
+			</div>
+		</x-grid.item>
+
+		<x-grid.item title="Icon Buttons">
+			<div class="flex-center">
+				<button class="neu-icon-btn neu-btn text-primary mr-3" type="button"><x-svg.sun class="size-7" /> Primary</button>
+				<button class="neu-icon-btn neu-btn text-secondary mr-3" type="button"><x-svg.moon class="size-7" /> Secondary</button>
+				<button class="neu-icon-btn neu-btn mr-3 text-green-700" type="button"><x-svg.mail class="size-7" /> Success</button>
+				<button class="neu-icon-btn neu-btn mr-3 text-red-700" type="button"><x-svg.heart class="size-7" /> Danger</button>
+				<button class="neu-icon-btn neu-btn text-primary active mr-3" type="button"><x-svg.truck class="size-7" /> Active</button>
+			</div>
+		</x-grid.item>
+
+		<x-grid.item title="Loading Buttons">
+			<div class="flex-center">
+				<button class="neu-btn neu-icon-only-btn text-primary mr-3" type="button"><x-svg.loading class="size-6"/></button>
+				<button class="neu-btn neu-icon-btn text-secondary mr-3" type="button"><x-svg.loading class="size-6"/>Loading...</button>
+			</div>
+		</x-grid.item>
 
 	</x-grid>
 
