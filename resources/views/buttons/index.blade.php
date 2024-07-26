fd0f21f64d7cd94a1f2f7f50e2087d98e390d704
Manish Tripathi <manish82644@gmail.com>
2024-07-26 16:37:28 +0530

    Refactor neumorphism design for forms: add Documents button component

--- resources/views/buttons/index.blade.php
+++ resources/views/buttons/index.blade.php
@@ -1,8 +1,18 @@
 <x-app-layout>
-	<div class="m-10 grid items-center justify-center">
 
-		<div>
+	<x-grid title="Buttons">
+		<x-slot name="description">
+			Buttons are used to interact with the user, they can be used to submit forms, navigate to other pages, or trigger actions.
+		</x-slot>
+
+		<x-grid.item title="Delete Button">
 			@include('buttons.del-btn')
-		</div>
-	</div>
+		</x-grid.item>
+
+		<x-grid.item title="Documents Button">
+			@include('buttons.docs-btn')
+		</x-grid.item>
+
+	</x-grid>
+
 </x-app-layout>
