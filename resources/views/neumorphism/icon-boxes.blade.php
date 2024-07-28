b7c56b45a0ad952535ef2a3836b3e0b5b608b339
Manish Tripathi <manish82644@gmail.com>
2024-07-28 13:19:12 +0530

    Refactor neumorphism design for forms: Add shield icons to icon boxes component

--- resources/views/neumorphism/icon-boxes.blade.php
+++ resources/views/neumorphism/icon-boxes.blade.php
@@ -101,7 +101,20 @@
 			</div>
 		</x-grid.item>
 
-		
+		<x-grid.item title="Shield Icons">
+			<div class="flex-center gap-3 md:gap-5">
+				<div class="flex-center text-secondary hover:text-primary rounded-shield border-2 border-light-secondary p-8 shadow-neu-inset-xs dark:border-dark-secondary dark:shadow-neu-dark-inset-xs">
+					<x-svg.ex class="size-7" />
+				</div>
+				<div class="flex-center text-secondary hover:text-primary rounded-shield border-2 border-light-secondary p-8 shadow-neu-xs dark:border-dark-secondary dark:shadow-neu-dark-xs">
+					<x-svg.ex class="size-7" />
+				</div>
+				<div class="flex-center text-secondary hover:text-primary rounded-shield border-2 border-light-secondary p-3 shadow-neu-inset-xs dark:border-dark-secondary dark:shadow-neu-dark-inset-xs">
+					<div class="rounded-shield border-2 border-light-secondary p-5 shadow-neu-xs dark:border-dark-secondary dark:shadow-neu-dark-xs"><x-svg.ex class="size-7" /></div>
+				</div>
+			</div>
+		</x-grid.item>
+
 	</x-grid>
 
 </x-app-layout>
