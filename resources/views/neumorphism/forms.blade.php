20a20c9dd4b69cec4fcfb4aaa36486575bd46c9d
Manish Tripathi <manish82644@gmail.com>
2024-07-30 22:48:03 +0530

    Refactor neumorphism design for forms: Update form input styles and add focus effects

--- resources/views/neumorphism/forms.blade.php
+++ resources/views/neumorphism/forms.blade.php
@@ -9,12 +9,12 @@
 			<form class="w-full md:w-2/3" action="#">
 				<div class="neu-form-group mb-4">
 					<label class="neu-form-label" for="email">Email address</label>
-					<input class="neu-form-input" id="email" type="email">
+					<input class="neu-form-input" id="email" type="email" placeholder="ashborn@godofdeath.sl">
 					<small class="neu-form-text" id="emailHelp">We'll never share your email with anyone else.</small>
 				</div>
 				<div class="neu-form-group mb-4">
 					<label class="neu-form-label" for="password">Password</label>
-					<input class="neu-form-input" id="password" type="password">
+					<input class="neu-form-input" id="password" type="password" placeholder="Password">
 				</div>
 				<div class="neu-form-group mb-4">
 					<label class="neu-form-checkbox-label" for="remember">
@@ -38,9 +38,9 @@
 					}
 
 					.neu-form-input {
-						@apply bg-primary text-secondary appearance-none rounded-lg border border-light-secondary px-3 py-2 font-karla shadow-neu-inset-sm outline-none placeholder:tracking-wide;
+						@apply bg-primary text-primary w-full active:placeholder:text-primary focus:placeholder:text-primary placeholder:text-secondary appearance-none rounded-lg border border-light-secondary px-3 py-2 font-karla outline-none placeholder:tracking-wide transition duration-300 shadow-neu-xs focus:shadow-neu-inset-xs;
 						/* dark mode */
-						@apply dark:shadow-neu-dark-inset-sm dark:border-dark-secondary;
+						@apply dark:focus:shadow-neu-dark-inset-xs dark:shadow-neu-dark-xs dark:border-dark-secondary;
 					}
 
 					.neu-form-checkbox-label {
@@ -85,26 +85,34 @@
 			<x-slot name="cssCode">
 				<style>
 					.neu-form-input {
-						@apply bg-primary w-full active:placeholder:text-primary focus:placeholder:text-primary placeholder:text-secondary text-secondary appearance-none rounded-lg border border-light-secondary px-3 py-2 font-karla shadow-neu-inset-sm outline-none placeholder:tracking-wide transition duration-300;
+						@apply bg-primary text-primary w-full active:placeholder:text-primary focus:placeholder:text-primary placeholder:text-secondary appearance-none rounded-lg border border-light-secondary px-3 py-2 font-karla outline-none placeholder:tracking-wide transition duration-300 shadow-neu-xs focus:shadow-neu-inset-xs;
 						/* dark mode */
-						@apply dark:shadow-neu-dark-inset-sm dark:border-dark-secondary;
+						@apply dark:focus:shadow-neu-dark-inset-xs dark:shadow-neu-dark-xs dark:border-dark-secondary;
 					}
 
-					/*-- ------ neu-input-group & Icon ------ */
+					/* ------------------------- neu-input-group & Icon ------------------------- */
 					.neu-input-group {
-						@apply flex w-full items-center rounded-lg border border-light-secondary dark:border-dark-secondary;
+						@apply flex w-full items-center rounded-lg border border-light-secondary dark:border-dark-secondary dark:shadow-neu-dark-xs shadow-neu-xs focus-within:shadow-none dark:focus-within:shadow-none;
 					}
 
 					.neu-input-icon {
-						@apply size-10 text-secondary border-r border-light-secondary px-2 py-1 dark:border-dark-secondary;
+						@apply size-10 text-secondary border-light-secondary px-2 py-1 dark:border-dark-secondary;
 					}
 
 					.neu-input-group-prepend .neu-form-input {
-						@apply rounded-r-lg rounded-l-none;
+						@apply rounded-r-lg rounded-l-none shadow-none focus:shadow-neu-inset-xs dark:focus:shadow-neu-dark-inset-xs;
+					}
+
+					.neu-input-group-prepend .neu-input-icon {
+						@apply rounded-l-lg rounded-r-none border-r;
 					}
 
 					.neu-input-group-append .neu-form-input {
-						@apply rounded-l-lg rounded-r-none;
+						@apply rounded-l-lg rounded-r-none shadow-none focus:shadow-neu-inset-xs dark:focus:shadow-neu-dark-inset-xs;
+					}
+
+					.neu-input-group-append .neu-input-icon {
+						@apply rounded-r-lg rounded-l-none border-l;
 					}
 
 					.neu-input-group .neu-form-input {
@@ -114,8 +122,12 @@
 					.neu-input-group:hover .neu-form-input,
 					.neu-input-group:focus .neu-form-input,
 					.neu-input-group:hover .neu-input-icon,
-					.neu-input-group:focus .neu-input-icon {
+					.neu-input-group:focus-within .neu-input-icon {
 						@apply placeholder:text-primary text-primary;
+					}
+
+					.neu-input-group:focus-within .neu-input-icon {
+						@apply shadow-neu-xs dark:shadow-neu-dark-xs;
 					}
 				</style>
 			</x-slot>
@@ -134,9 +146,11 @@
 			<x-slot name="cssCode">
 				<style>
 					.neu-form-select {
-						@apply appearance-none bg-primary text-secondary rounded-lg border border-light-secondary px-3 py-2 font-karla shadow-neu-inset-sm outline-none transition duration-300 bg-no-repeat pr-10;
+						@apply bg-primary text-primary w-full active:placeholder:text-primary focus:placeholder:text-primary placeholder:text-secondary appearance-none rounded-lg border border-light-secondary px-3 py-2 font-karla outline-none placeholder:tracking-wide transition duration-300 shadow-neu-xs focus:shadow-neu-inset-xs;
 						/* dark mode */
-						@apply dark:shadow-neu-dark-inset-sm dark:border-dark-secondary;
+						@apply dark:focus:shadow-neu-dark-inset-xs dark:shadow-neu-dark-xs dark:border-dark-secondary;
+						/* arrow */
+						@apply appearance-none bg-no-repeat pr-10;
 						background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="%23767676" d="M8.037 11.166L14.5 22.36c.825 1.43 2.175 1.43 3 0l6.463-11.195c.826-1.43.15-2.598-1.5-2.598H9.537c-1.65 0-2.326 1.17-1.5 2.6z"/></svg>');
 						background-position: right 0.5rem center;
 						background-size: 1.5rem 1.5rem;
@@ -154,9 +168,9 @@
 			<x-slot name="cssCode">
 				<style>
 					.neu-form-textarea {
-						@apply bg-primary w-full transition-all duration-500 ease-Out-expo active:placeholder:text-primary focus:placeholder:text-primary placeholder:text-secondary text-secondary appearance-none rounded-lg border border-light-secondary px-3 py-2 font-karla shadow-neu-inset-sm outline-none placeholder:tracking-wide;
+						@apply bg-primary text-primary w-full active:placeholder:text-primary transition-all duration-500 ease-Out-expo focus:placeholder:text-primary placeholder:text-secondary appearance-none rounded-lg border border-light-secondary px-3 py-2 font-karla outline-none placeholder:tracking-wide shadow-neu-xs focus:shadow-neu-inset-xs;
 						/* dark mode */
-						@apply dark:shadow-neu-dark-inset-sm dark:border-dark-secondary;
+						@apply dark:focus:shadow-neu-dark-inset-xs dark:shadow-neu-dark-xs dark:border-dark-secondary;
 					}
 
 					.scrollbar-thin {
