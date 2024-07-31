6219b8d3c458bcfba148eea3543cd80ac6e775a3
Manish Tripathi <manish82644@gmail.com>
2024-07-31 14:12:11 +0530

    Refactor neumorphism design: Add rounded pagination

--- resources/views/neumorphism/pagination.blade.php
+++ resources/views/neumorphism/pagination.blade.php
@@ -89,6 +89,50 @@
 			</div>
 		</x-grid.item>
 
+		<x-grid.item title="Rounded Pagination">
+			<div class="bg-primary rounded-full p-3 shadow-neu-xs">
+				<ul class="flex-center list-none">
+					<li class="bg-primary mx-1 flex-1 rounded-full shadow-neu-xs first:mr-2 last:ml-2 sm:first:mr-4 sm:last:ml-4">
+						<a class="text-secondary hover:text-primary block px-2 py-1 font-karla no-underline" href="#">
+							<x-svg.left class="size-6" />
+						</a>
+					</li>
+					<li class="bg-primary mx-1 flex-1 rounded-full shadow-neu-xs">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">1</a>
+					</li>
+					<li class="bg-primary mx-1 flex-1 rounded-full shadow-neu-xs">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">2</a>
+					</li>
+					<li class="bg-primary mx-1 hidden flex-1 rounded-full shadow-neu-xs sm:block">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">3</a>
+					</li>
+					<li class="bg-primary mx-1 hidden flex-1 rounded-full shadow-neu-xs sm:block">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">4</a>
+					</li>
+					<li class="bg-primary mx-1 hidden flex-1 rounded-full shadow-neu-xs sm:block">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">5</a>
+					</li>
+					<li class="bg-primary mx-1 hidden flex-1 rounded-full shadow-neu-xs sm:block">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">6</a>
+					</li>
+					<li class="bg-primary mx-1 hidden flex-1 rounded-full shadow-neu-xs sm:block">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">7</a>
+					</li>
+					<li class="bg-primary mx-1 hidden flex-1 rounded-full shadow-neu-xs sm:block">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">8</a>
+					</li>
+					<li class="bg-primary mx-1 flex-1 rounded-full shadow-neu-xs sm:hidden">
+						<a class="text-secondary hover:text-primary min-w-7 block p-1 text-center font-karla no-underline" href="#">...</a>
+					</li>
+					<li class="bg-primary mx-1 flex-1 rounded-full shadow-neu-xs first:mr-2 last:ml-2 sm:first:mr-4 sm:last:ml-4">
+						<a class="text-secondary hover:text-primary block px-2 py-1 text-center font-karla no-underline" href="#">
+							<x-svg.right class="size-6" />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</x-grid.item>
+
 	</x-grid>
 
 </x-app-layout>
