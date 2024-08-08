b74e3fd9ba1f5475f1034bdbc07e5b681da22e67
Manish Tripathi <manish82644@gmail.com>
2024-08-08 13:40:02 +0530

    chore: correct image-card

--- resources/views/cards/image-card.blade.php
+++ resources/views/cards/image-card.blade.php
@@ -7,47 +7,35 @@
 				<h3 class="mb-4 text-2xl">
 					Make your <span class="text-green-700">choice</span> right now!
 				</h3>
-				<p class="text-xs">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla a aut?</p>
+				<p class="text-xs">Join the adventures of Naruto Uzumaki as he strives to become Hokage.</p>
 				<a class="group-hover absolute bottom-4 left-2/4 flex -translate-x-2/4 translate-y-[10%] flex-wrap items-center p-2.5 text-green-700 no-underline opacity-0 transition-all duration-[0.35s] group-hover:opacity-100" href="#">
 					<span>Learn How</span>
-					<svg class="ml-1 w-4 transition-transform duration-[0.3s] group-hover:translate-x-1" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-					</svg>
+					<x-svg.naruto class="ml-1 w-4 transition-transform duration-[0.3s] group-hover:translate-x-1" />
 				</a>
 			</div>
-			<img class="absolute left-0 top-0 z-[-1] h-full w-full scale-[1.2] object-cover object-center transition-transform duration-[0.35s] ease-[cubic-bezier(0.1,0.72,0.4,0.97)] group-hover:scale-100" src="{{ asset('images/drink.png') }}" alt="">
+			<img class="absolute left-0 top-0 z-[-1] h-full w-full scale-[1.2] object-cover object-center transition-transform duration-[0.35s] ease-[cubic-bezier(0.1,0.72,0.4,0.97)] group-hover:scale-100" src="{{ asset('images/naruto.png') }}" alt="Naruto Uzumaki">
 		</div>
 	@break
 
 	@case(2)
 		<div class="w-5/6 max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
-			<div class="relative overflow-hidden rounded-lg bg-white p-5 shadow-sm transition duration-150 ease-in-out focus-within:translate-y-[-5px] focus-within:shadow-md hover:translate-y-[-5px] hover:shadow-md">
+			<div class="bg-primary relative overflow-hidden rounded-lg p-5 shadow-md dark:shadow-gray-600 transition duration-150 ease-in-out focus-within:translate-y-[-5px] focus-within:shadow-lg hover:translate-y-[-5px] hover:shadow-lg">
 				<figure class="overflow-hidden rounded-lg">
-					<img class="block max-w-full" src="{{ asset('images/spider.png') }}" alt="" />
+					<img class="block max-w-full" src="{{ asset('images/landscape/one_piece.png') }}" alt="One Piece" />
 				</figure>
 				<div class="mt-6 flex items-center justify-between">
-					<a class="pr-4 text-lg font-semibold leading-tight text-gray-900 dark:text-white no-underline hover:text-primary focus:text-primary" href="#">When life gives you spiders</a>
-					<button class="duration-250 z-10 flex h-10 w-10 items-center justify-center rounded-full bg-white text-xl text-gray-600 shadow-sm transition ease-in-out hover:bg-red-500 hover:text-white focus:bg-red-500 focus:text-white">
-						<svg class="h-6 w-6" id="Heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" display="block">
-							<path d="M7 3C4.239 3 2 5.216 2 7.95c0 2.207.875 7.445 9.488 12.74a.985.985 0 0 0 1.024 0C21.125 15.395 22 10.157 22 7.95 22 5.216 19.761 3 17 3s-5 3-5 3-2.239-3-5-3z" />
-						</svg>
+					<a class="text-primary pr-4 text-lg font-semibold leading-tight text-gray-900 no-underline" href="#">Join Luffy's Adventure</a>
+					<button class="duration-250 bg-primary z-10 flex h-10 w-10 items-center justify-center rounded-full text-xl text-gray-600 shadow-sm transition ease-in-out hover:bg-red-500 hover:text-white focus:bg-red-500 focus:text-white">
+						<x-svg.luffy class="h-6 w-6" />
 					</button>
 				</div>
-				<div class="mt-5 flex flex-wrap items-center border-t border-gray-300 pt-5">
-					<div class="flex items-center text-primary">
-						<svg class="mr-2 h-6 w-6" id="EyeOpen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" display="block">
-							<path d="M21.257 10.962c.474.62.474 1.457 0 2.076C19.764 14.987 16.182 19 12 19c-4.182 0-7.764-4.013-9.257-5.962a1.692 1.692 0 0 1 0-2.076C4.236 9.013 7.818 5 12 5c4.182 0 7.764 4.013 9.257 5.962z" />
-							<circle cx="12" cy="12" r="3" />
-						</svg>
+				<div class="mt-5 flex flex-wrap items-center border-t border-gray-600 pt-5">
+					<div class="text-primary flex items-center">
+						<x-svg.zoro class="mr-2 h-6 w-6" />
 						<span>2,465</span>
 					</div>
-					<div class="ml-3 flex items-center text-primary">
-						<svg class="mr-2 h-6 w-6" id="Calendar" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" display="block">
-							<rect x="2" y="4" width="20" height="18" rx="4" />
-							<path d="M8 2v4" />
-							<path d="M16 2v4" />
-							<path d="M2 10h20" />
-						</svg>
+					<div class="text-primary ml-3 flex items-center">
+						<x-svg.calendar class="mr-2 h-6 w-6" />
 						<span>Jul 26, 2019</span>
 					</div>
 				</div>
--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -1,50 +1,52 @@
 <x-app-layout title="Cards">
-	<div class="flex flex-col items-center justify-center gap-y-5 p-5">
-		<x-grid title="Cards">
+	<x-grid title="Cards">
 
-			<x-slot name="description">
-				<x-joke />
-			</x-slot>
+		<x-slot name="description">
+			Card components are used to display information in a structured manner. They are used to display content in a more organized and visually appealing way. Cards are used to display information in a structured manner. They are used to display content in a more organized and visually appealing way.
+		</x-slot>
 
-			<x-grid.item title="Basic Card Component">
-				@include('cards.image-card', ['type' => 2])
-			</x-grid.item>
+		<x-grid.item title="Badge Card">
+			@include('cards.badge-card')
+		</x-grid.item>
 
-			<x-grid.item title="Image Card Component">
-				@include('cards.image-card', ['type' => 1])
-			</x-grid.item>
+		<x-grid.item title="Basic Card">
+			@include('cards.image-card', ['type' => 2])
+		</x-grid.item>
 
-			<x-grid.item title="Badge Card Component">
-				@include('cards.badge-card')
-			</x-grid.item>
+		<x-grid.item title="Comment Card">
+			@include('cards.comment')
+		</x-grid.item>
 
-			<x-grid.item title="Icon Card Component">
-				@include('cards.icon-card')
-			</x-grid.item>
+		<x-grid.item title="Icon Card">
+			@include('cards.icon-card')
+		</x-grid.item>
 
-			<x-grid.item title="Notebook Page Card Component">
-				@include('cards.notebook-page-card')
-			</x-grid.item>
+		<x-grid.item title="Image Card">
+			@include('cards.image-card', ['type' => 1])
+		</x-grid.item>
 
-			<x-grid.item title="Notebook Page Card Component 2">
-				@include('cards.notebook-page-card', ['type' => 1])
-			</x-grid.item>
+		<x-grid.item title="Notebook Page Card">
+			@include('cards.notebook-page-card')
+		</x-grid.item>
 
-			<x-grid.item title="Reflect Hover Card Component">
-				@include('cards.reflect-hover')
-			</x-grid.item>
+		<x-grid.item title="Notebook Page Card 2">
+			@include('cards.notebook-page-card', ['type' => 1])
+		</x-grid.item>
 
-		</x-grid>
-	</div>
+		<x-grid.item title="Reflect Hover Card">
+			@include('cards.reflect-hover')
+		</x-grid.item>
+	</x-grid>
 
 	{{-- Right Sidenav --}}
 	<x-slot name="rightSidenav">
-		<x-sidenav-list>Basic Card Component</x-sidenav-list>
-		<x-sidenav-list>Image Card Component</x-sidenav-list>
-		<x-sidenav-list>Badge Card Component</x-sidenav-list>
-		<x-sidenav-list>Icon Card Component</x-sidenav-list>
-		<x-sidenav-list>Notebook Page Card Component</x-sidenav-list>
-		<x-sidenav-list>Notebook Page Card Component 2</x-sidenav-list>
-		<x-sidenav-list>Reflect Hover Card Component</x-sidenav-list>
+		<x-sidenav-list>Badge Card</x-sidenav-list>
+		<x-sidenav-list>Basic Card</x-sidenav-list>
+		<x-sidenav-list>Comment Card</x-sidenav-list>
+		<x-sidenav-list>Icon Card</x-sidenav-list>
+		<x-sidenav-list>Image Card</x-sidenav-list>
+		<x-sidenav-list>Notebook Page Card</x-sidenav-list>
+		<x-sidenav-list>Notebook Page Card 2</x-sidenav-list>
+		<x-sidenav-list>Reflect Hover Card</x-sidenav-list>
 	</x-slot>
 </x-app-layout>
