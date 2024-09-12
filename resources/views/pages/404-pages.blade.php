9aaa0d2afb81464fd65f5359c4d8d92bf73329b6
Manish Tripathi <manish82644@gmail.com>
2024-09-13 00:06:27 +0530

    Refactor 404 page to display user-friendly message and add new inspirational pages

--- resources/views/pages/404-pages.blade.php
+++ resources/views/pages/404-pages.blade.php
@@ -2,10 +2,29 @@
 
 	<x-grid title="404">
 		<x-slot name="description">
-
+			404 pages are displayed when a user tries to access a page that does not exist. They are used to inform the user that the page they are looking for cannot be found.
 		</x-slot>
 
-		<x-grid.item title="item title">
+		<x-grid.item title="Simple">
+			<!-- component -->
+			<div class="bg-primary flex h-screen w-full flex-col items-center justify-center">
+				<h1 class="text-primary text-9xl font-extrabold tracking-widest">404</h1>
+				<div class="bg-accent absolute rotate-12 rounded px-2 text-sm">
+					Page Not Found
+				</div>
+				<button class="mt-5">
+					<a class="text-accent group relative inline-block text-sm font-medium focus:outline-none focus:ring active:text-orange-500">
+						<span class="bg-accent absolute inset-0 translate-x-0.5 translate-y-0.5 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
+
+						<span class="bg-primary relative block border border-current px-8 py-3">
+							<router-link to="/">Go Home</router-link>
+						</span>
+					</a>
+				</button>
+			</div>
+		</x-grid.item>
+
+		<x-grid.item title="Bouncing Emoji Page">
 			<div class="bg-primary grid place-items-center">
 				<svg class="animation-duration-3000 mt-24 animate-bounce" id="Layer_1" enable-background="new 0 0 226 249.135" height="249.135" overflow="visible" version="1.1" viewBox="0 0 226 249.135" width="226" xml:space="preserve">
 					<circle cx="113" cy="113" fill="#FFE585" r="109" />
@@ -27,6 +46,24 @@
 				</div>
 
 				<x-primary-btn class="mt-6">Go back </x-primary-btn>
+			</div>
+		</x-grid.item>
+
+		<x-grid.item title="Alone Man in Space">
+			<div class="relative h-screen w-full overflow-hidden bg-indigo-900">
+				<img class="absolute h-full w-full object-cover" src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9" />
+				<div class="absolute inset-0 bg-black opacity-25">
+				</div>
+				<div class="container relative z-10 mx-auto flex items-center px-6 py-32 md:px-12 xl:py-40">
+					<div class="relative z-10 flex w-full flex-col items-center font-mono">
+						<h1 class="mt-4 text-center text-5xl font-extrabold leading-tight text-white">
+							You are all alone here
+						</h1>
+						<p class="my-44 animate-bounce text-8xl font-extrabold text-white">
+							404
+						</p>
+					</div>
+				</div>
 			</div>
 		</x-grid.item>
 
