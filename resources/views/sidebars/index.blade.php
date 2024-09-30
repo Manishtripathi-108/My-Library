7dda579046f9ea4459d5e0ffbd0dffe24245ee46
Manish Tripathi <manish82644@gmail.com>
2024-09-30 23:00:50 +0530

    Refactor sidebar index and add new sidebar with icons

--- resources/views/sidebars/index.blade.php
+++ resources/views/sidebars/index.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout title="Sidebars">
 
-    <x-grid title="Sidebars">
-        <x-slot name="description">
-            Sidebars are used to display additional information or options that are not part of the main content. They are used to provide users with quick access to important information or features.
-        </x-slot>
-    
-        <x-grid.item title="Simple Sidebar">
-            @include('sidebars.simple-sidebar')
-        </x-grid.item>
-    
-    </x-grid>
+	<x-grid title="Sidebars">
+		<x-slot name="description">
+			Sidebars are used to display additional information or options that are not part of the main content. They are used to provide users with quick access to important information or features.
+		</x-slot>
+
+		<x-grid.item title="Simple Sidebar">
+			@include('sidebars.simple-sidebar')
+		</x-grid.item>
+
+		<x-grid.item title="Sidebar with Icons">
+			@include('sidebars.sidebar-with-icons')
+		</x-grid.item>
+
+	</x-grid>
 
 </x-app-layout>
